843dfc2f7cc69242241b128aa24f51cbbff8c603
kristianpayne1 <kjp29@kent.ac.uk>
2019-09-04 20:29:37 +0100

    Removed procedural generation, proved too complex

--- KS Ski/Assets/GameManager.cs
+++ KS Ski/Assets/GameManager.cs
@@ -6,6 +6,27 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     bool gameEnded = false;
+    [SerializeField]
+    private int boundary;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private GameObject camera;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = player.transform;
+    }
+
+    void Update()
+    {
+        if(playerTransform.position.x > boundary || playerTransform.position.z > boundary
+            || -playerTransform.position.x < -boundary || -playerTransform.position.z < -boundary)
+        {
+            rerootWorld();
+        }
+    }
 
     public void endGame()
     {
@@ -32,5 +53,21 @@ public class GameManager : MonoBehaviour
 
     public void resumeGame()
     {
-        Time.timeScale = 1;    }
+        Time.timeScale = 1;    
+    }
+
+    private void rerootWorld()
+    {
+        Debug.Log("Re-rooting world");
+        Vector3 cameraOffset = camera.GetComponent<FollowPlayer>().getCurrentOffset();
+        player.SetActive(false);
+        TrailRenderer[] trails = player.GetComponentsInChildren<TrailRenderer>();
+        foreach (TrailRenderer t in trails)
+        {
+            t.Clear();
+        }
+        playerTransform.position = new Vector3(0,playerTransform.position.y,0);
+        player.SetActive(true);
+        camera.transform.position = new Vector3(playerTransform.position.x+cameraOffset.x, playerTransform.position.y+cameraOffset.y, playerTransform.position.z+cameraOffset.z);
+    }
 }
